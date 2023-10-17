c84c76f8725f91df3d1e8debb5cdb178ae0e9020
Константин Тимофеев <kt@webink.site>
2023-10-17 22:45:07 +0300

    fix logic

--- index.js
+++ index.js
@@ -5,61 +5,57 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 let mainResult = []
 
+// basic comands
 bot.command('start', (ctx) => {
     ctx.reply('Привет! Укажи через «-» вес и кол-во раз, которое ты можешь сделать')
-    // {
-    //     reply_markup: Markup.inlineKeyboard([
-    //         Markup.callbackButton('Кнопка 1', 'one')
-    //     ])
-    // }
 })
 
 bot.command('help', (ctx) => {
     ctx.replyWithHTML('+79643843454 - <b>Тинькофф</b>(Константин Т)')
 })
 
-// bot.action('one', (ctx) => {
-//     ctx.reply('Вы нажали на 1-ю кнопку')
-// })
+// logic
 
 bot.on('text', (ctx) => {
     const res = ctx.message.text.split('-')
-    console.log(res)
     if(res.length < 2){
         return
     }
-    mainResult = res
+    const userInStore = mainResult.find(i => i.user_id === ctx.from.id)
+    if(!userInStore){
+        mainResult.push({user_id: ctx.from.id, val: res})
+    }else{
+        userInStore = {user_id: ctx.from.id, val: res}
+    }
+
     ctx.reply(`Ты жмешь ${res[0]}кг - ${res[1]} раз?`, {
         reply_markup: Markup.inlineKeyboard([
             Markup.callbackButton('Да, верно!', 'success'),
             Markup.callbackButton('Нет', 'error')
         ])
     })
-
-    // ctx.reply('Как дела?', {
-    //     reply_markup: Markup.keyboard([['Отлично', 'Плохо']])
-    // })
 })
 
 
 bot.action('success', (ctx) => {
-    if(mainResult.length == 0){
+    const savedInStore = mainResult.find((i) => i.user_id === ctx.from.id)
+    console.log(savedInStore)
+    if(!savedInStore){
         ctx.reply('Укажи через «-» вес и кол-во раз, которое ты можешь сделать')
         return
+    }else{
+        const weight = savedInStore.val[0]
+        const repeats = savedInStore.val[1]
+        const total = (-1.89 ) + (1.16 * weight) + (1.68 * repeats)
+        const idx = mainResult.indexOf(savedInStore)
+        ctx.reply(`На раз ты пожмешь ${parseFloat(total).toFixed(1)}кг`)
+        mainResult.splice(idx, 1)
     }
-    const weight = mainResult[0]
-    const repeats = mainResult[1]
-    const total = (-1.89 ) + (1.16 * weight) + (1.68 * repeats)
-    // где M-вес штанги, k-количество повторов с этим весом
-    ctx.reply(`На раз ты пожмешь ${parseFloat(total).toFixed(1)}кг`)
-    mainResult = []
 })
 
 bot.action('error', (ctx) => {
     ctx.reply('Укажи через «-» вес и кол-во раз, которое ты можешь сделать')
     mainResult = []
 })
-
-
 
 bot.launch()
